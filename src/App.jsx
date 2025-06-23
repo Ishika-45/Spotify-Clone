c4b2c22ad030216b0f65f3acde71f8790a652919
Ishika-45 <bansalishika58@gmail.com>
2025-06-23 17:59:07 +0530

    Update something

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Callback from "./components/Callback";
-
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Callback from './components/Callback';
 
 export default function App() {
+  const [accessToken, setAccessToken] = useState(null);
+
   useEffect(() => {
-    const hash = window.location.hash;
-    console.log(hash);
-    if(hash) {
-      const token = hash.substring(1).split("&")[0].split('');
-      console.log(token);
+    // 1. Check if token already exists in localStorage
+    const storedToken = localStorage.getItem('access_token');
+
+    if (storedToken) {
+      setAccessToken(storedToken);
     }
-  },[]);
+
+  }, []);
+
+  // You can now use `accessToken` anywhere in the app
+  console.log("Access Token:", accessToken);
 
   return (
- <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/callback" element={<Callback />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
