62da8f4bfe0aa9d89615fd81953c33bbe4e2f65b
Ishika-45 <bansalishika58@gmail.com>
2025-06-23 16:59:57 +0530

    login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,12 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 
 export default function Login() {
+
+    const handleCLick = () => {
+        const clientId = "f4a151fe857e45a9b788201b0f9cb173";
+        const redirectUrl = "https://spotify-clone-murex-eight-39.vercel.app/";
+        const apiUrl = "https://accounts.spotify.com/authorize";
+        const scope = [
+            "user-read-email",
+            "user-read-private",
+            // "user-personalized",
+            "user-read-playback-state",
+            "user-modify-playback-state",
+            "user-read-currently-playing",
+            "user-read-playback-position",
+            "user-top-read",
+            "user-read-recently-played",
+        ];
+        window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(" ")}&response_type=token&show_dialog=true`;
+
+
+    }
+
     return (
         <Container>
-            <img src="Spotify.png" alt="spotify" />
-            <button>Connect Spotify</button>
+            <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Full_Logo_RGB_Black.png" alt="spotify" />
+
+            <button onClick={handleCLick}>Connect Spotify</button>
         </Container>
-    )
+    );
 }
 
 const Container = styled.div`
@@ -16,7 +38,7 @@ const Container = styled.div`
     align-items: center;
     justify-content: center;
     height: 100vh;
-    width: 100vw;
+    width: calc(100vw - 12px);
     background-color:rgb(164, 99, 239);
     gap: 5rem;
     img{
@@ -29,6 +51,7 @@ const Container = styled.div`
     background-color: black;
     color: rgb(164, 99, 239);
     font-size: 1.4rem;
+    cursor: pointer;
     }
 
 `;
