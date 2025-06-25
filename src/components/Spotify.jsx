dadd84461129514e2f0de02e0fe31ead66733d70
Ishika-45 <bansalishika58@gmail.com>
2025-06-25 23:28:37 +0530

    fixing errors

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
