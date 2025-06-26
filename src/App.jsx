220422e676dc0525bbd5b26fa000cf97cf89d4b2
Ishika-45 <bansalishika58@gmail.com>
2025-06-26 11:29:06 +0530

    fixing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-
-
 // src/App.jsx
 import React, { useEffect } from "react";
 import Login from "./components/Login";
 import Spotify from "./components/Spotify";
-import { reducerCases } from "./utils/Constants";
 import { useStateProvider } from "./utils/StateProvider";
+import { reducerCases } from "./utils/Constants";
 
 export default function App() {
   const [{ token }, dispatch] = useStateProvider();
 
   useEffect(() => {
-    // Get token from URL hash (after Spotify login)
     const hash = window.location.hash;
-    if (hash) {
-      const extractedToken = hash.substring(1).split("&")[0].split("=")[1];
-      if (extractedToken) {
-        dispatch({ type: reducerCases.SET_TOKEN, token: extractedToken });
-        localStorage.setItem("access_token", extractedToken);
-        window.location.hash = ""; // clean up URL
+    let accessToken = localStorage.getItem("access_token");
+
+    // ✅ Extract token from URL fragment after login
+    if (!accessToken && hash) {
+      const params = new URLSearchParams(hash.substring(1));
+      accessToken = params.get("access_token");
+
+      if (accessToken) {
+        localStorage.setItem("access_token", accessToken);
+        dispatch({ type: reducerCases.SET_TOKEN, token: accessToken });
+        window.location.hash = ""; // Clean URL
       }
     }
 
-    // If reloading, get token from localStorage
-    const storedToken = localStorage.getItem("access_token");
-    if (storedToken && !token) {
-      dispatch({ type: reducerCases.SET_TOKEN, token: storedToken });
+    // ✅ If token already in storage but not in context
+    if (accessToken && !token) {
+      dispatch({ type: reducerCases.SET_TOKEN, token: accessToken });
     }
 
     document.title = "Spotify";
