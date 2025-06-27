3f208ac3da2ec7bea2df54239c69cd342aafd184
Ishika-45 <bansalishika58@gmail.com>
2025-06-27 11:12:36 +0530

    Fixing errors

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -97,7 +97,7 @@ export default function Playlists() {
         );
         const { items } = response.data;
         const playlists = items.map(({ name, id,image }) => {
-          return { name, id, image: images?.[0]?.url || "", };
+          return { name, id, image: image?.[0]?.url || "", };
         });
         dispatch({ type: reducerCases.SET_PLAYLISTS, playlists });
       } catch (error) {
